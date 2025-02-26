c8d7f66734efc767c78338e7af0ba592c3824b3e
Moritz Räpple <moritz.raepple@deutschebahn.com>
2025-02-26 16:01:55 +0100

    fix: Update customers client to properly parse response

--- internal/clients/customers/customers.go
+++ internal/clients/customers/customers.go
@@ -1,20 +1,31 @@
 package customers
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log/slog"
 	"net/http"
 
-	github.com/codevibe-de/goadv--orders/internal/model
+	"github.com/codevibe-de/goadv--orders/internal/model"
 )
 
-const customersURL = "http://localhost:8081/"
+const (
+	customersURL = "http://localhost:8081/"
+	serverPort   = ":8081"
+)
 
-func GetCustomerByPhoneNumber(phoneNumber string) model.Customer {
-	requestURL := fmt.Sprintf(customersURL + "customer/" + phoneNumber, serverPort)
+func GetCustomerByPhoneNumber(phoneNumber string, logger *slog.Logger) (*model.Customer, error) {
+	requestURL := fmt.Sprintf(customersURL+"customer/"+phoneNumber, serverPort)
 	res, err := http.Get(requestURL)
-
+	if err != nil {
+		logger.Error("Failed to get customer", "err", err.Error())
+		return nil, err
+	}
+	defer res.Body.Close()
 	c := model.Customer{}
-	json.Decoder(res.Body).Decode(&c)
+	body, err := io.ReadAll(res.Body)
+	json.Unmarshal(body, &c)
 
-	return &c
+	return &c, nil
 }
